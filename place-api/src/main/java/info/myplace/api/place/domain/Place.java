1906c028af2f713df74dc6f05c9c9bfa223e299f
Jeongsu Yang <mycat83@gmail.com>
2020-11-28 23:01:37 +0900

    [#2] 엔티티 클래스 생성

---
+++ place-api/src/main/java/info/myplace/api/place/domain/Place.java
@@ -0,0 +1,18 @@
+package info.myplace.api.place.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Place extends BaseEntity {}
---
+++ place-api/src/main/java/info/myplace/api/place/domain/PlaceHour.java
@@ -0,0 +1,13 @@
+package info.myplace.api.place.domain;
+
+import lombok.Builder;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlaceHour extends BaseEntity {}

6b237a7efa128bbb92f3a4fdbb1f0132a2cfa103
Jeongsu Yang <mycat83@gmail.com>
2020-12-14 22:55:05 +0900

    [#9] Controller 테스트 방식 변경

--- place-api/src/test/java/info/myplace/api/place/controller/TagControllerTest.java
+++ place-api/src/test/java/info/myplace/api/place/controller/TagControllerTest.java
@@ -1,28 +1,34 @@
 package info.myplace.api.place.controller;
 
-import info.myplace.api.place.domain.Tag;
 import info.myplace.api.place.dto.TagDto;
-import info.myplace.api.place.repository.TagRepository;
+import info.myplace.api.place.service.TagService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
-@AutoConfigureWebTestClient
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(TagController.class)
 @ActiveProfiles("test")
 @DisplayName("TagController 클래스")
 class TagControllerTest {
 
   @Autowired private WebTestClient webTestClient;
-  @Autowired private TagRepository tagRepository;
+
+  @MockBean private TagService tagService;
 
   @Nested
   @DisplayName("POST /tag 요청은")
@@ -34,6 +40,7 @@ class TagControllerTest {
 
       // Given
       TagDto tagDto = TagDto.builder().name("태그").build();
+      given(tagService.create(tagDto)).willReturn(Mono.just(tagDto));
 
       // When
       WebTestClient.ResponseSpec responseSpec =
@@ -43,85 +50,87 @@ class TagControllerTest {
       responseSpec
           .expectStatus()
           .isCreated()
+          .expectHeader()
+          .contentType(MediaType.APPLICATION_JSON)
           .expectBody()
-          .jsonPath("$.id")
-          .isNumber()
           .jsonPath("$.name")
           .isEqualTo(tagDto.getName());
+
+      verify(tagService, times(1)).create(any());
     }
   }
 
-  @Nested
-  @DisplayName("GET /tag 요청은")
-  class Get {
-
-    @Test
-    @DisplayName("id를 요청받아서 조회한 dto를 리턴한다")
-    void get() {
-
-      // Given
-      Tag tag = tagRepository.save(Tag.builder().name("태그").build());
-
-      // When
-      WebTestClient.ResponseSpec responseSpec =
-          webTestClient.get().uri("/tag/{id}", tag.getId()).exchange();
-
-      // Then
-      responseSpec
-          .expectStatus()
-          .isOk()
-          .expectBody()
-          .jsonPath("$.id")
-          .isEqualTo(tag.getId())
-          .jsonPath("$.name")
-          .isEqualTo(tag.getName());
-    }
-
-    @Test
-    @DisplayName("keyword를 요청받아서 조회한 dto 리스트를 리턴한다")
-    void getByKeyword() {
-
-      // Given
-      Tag tag = tagRepository.save(Tag.builder().name("태그1").build());
-      tagRepository.save(Tag.builder().name("태그2").build());
-
-      // When
-      WebTestClient.ResponseSpec responseSpec =
-          webTestClient
-              .get()
-              .uri(
-                  uriBuilder ->
-                      uriBuilder
-                          .path("/tag")
-                          .queryParam("keyword", tag.getName().substring(0, 1))
-                          .build())
-              .exchange();
-
-      // Then
-      responseSpec.expectStatus().isOk().expectBody().jsonPath("$", hasSize(2));
-    }
-  }
-
-  @Nested
-  @DisplayName("DELETE /tag 요청은")
-  class Delete {
-
-    @Test
-    @DisplayName("dto를 요청받아서 entity를 삭제한다")
-    void delete() {
-
-      // Given
-      Tag tag = tagRepository.save(Tag.builder().name("태그").build());
-
-      // When
-      WebTestClient.ResponseSpec responseSpec =
-          webTestClient.delete().uri("/tag/{id}", tag.getId()).exchange();
-
-      // Then
-      responseSpec.expectStatus().isOk();
-      if (tagRepository.findById(tag.getId()).isPresent()) {
-        throw new AssertionError("Test failed");
-      }
-    }
-  }
+  //  @Nested
+  //  @DisplayName("GET /tag 요청은")
+  //  class Get {
+  //
+  //    @Test
+  //    @DisplayName("id를 요청받아서 조회한 dto를 리턴한다")
+  //    void get() {
+  //
+  //      // Given
+  //      Tag tag = tagRepository.save(Tag.builder().name("태그").build());
+  //
+  //      // When
+  //      WebTestClient.ResponseSpec responseSpec =
+  //          webTestClient.get().uri("/tag/{id}", tag.getId()).exchange();
+  //
+  //      // Then
+  //      responseSpec
+  //          .expectStatus()
+  //          .isOk()
+  //          .expectBody()
+  //          .jsonPath("$.id")
+  //          .isEqualTo(tag.getId())
+  //          .jsonPath("$.name")
+  //          .isEqualTo(tag.getName());
+  //    }
+  //
+  //    @Test
+  //    @DisplayName("keyword를 요청받아서 조회한 dto 리스트를 리턴한다")
+  //    void getByKeyword() {
+  //
+  //      // Given
+  //      Tag tag = tagRepository.save(Tag.builder().name("태그1").build());
+  //      tagRepository.save(Tag.builder().name("태그2").build());
+  //
+  //      // When
+  //      WebTestClient.ResponseSpec responseSpec =
+  //          webTestClient
+  //              .get()
+  //              .uri(
+  //                  uriBuilder ->
+  //                      uriBuilder
+  //                          .path("/tag")
+  //                          .queryParam("keyword", tag.getName().substring(0, 1))
+  //                          .build())
+  //              .exchange();
+  //
+  //      // Then
+  //      responseSpec.expectStatus().isOk().expectBody().jsonPath("$", hasSize(2));
+  //    }
+  //  }
+  //
+  //  @Nested
+  //  @DisplayName("DELETE /tag 요청은")
+  //  class Delete {
+  //
+  //    @Test
+  //    @DisplayName("dto를 요청받아서 entity를 삭제한다")
+  //    void delete() {
+  //
+  //      // Given
+  //      Tag tag = tagRepository.save(Tag.builder().name("태그").build());
+  //
+  //      // When
+  //      WebTestClient.ResponseSpec responseSpec =
+  //          webTestClient.delete().uri("/tag/{id}", tag.getId()).exchange();
+  //
+  //      // Then
+  //      responseSpec.expectStatus().isOk();
+  //      if (tagRepository.findById(tag.getId()).isPresent()) {
+  //        throw new AssertionError("Test failed");
+  //      }
+  //    }
+  //  }
 }
